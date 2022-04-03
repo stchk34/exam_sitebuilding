d7db94a76a2edb67d0809b94dda0490c1d80adac
stchk34 <sandratkachukk@gmail.com>
2022-04-03 19:27:20 +0300

    add

---
+++ web/sites/default/files/php/twig/624a48dc33487_block--system-branding-bl_CTQSoBRNbLXaKp1e4VOJ5H8ui/uN200AeFqs2SWmCOFnWMUfZP7IyuNqGPhS3BuViRHo8.php
@@ -0,0 +1,109 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/exam/templates/block--system-branding-block.html.twig */
+class __TwigTemplate_0a6ba5ccfa4c63ee945beae6379d31bfe09c0fd24f3c65cc3aa75be9dbd686d6 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 15
+        $context["attributes"] = twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "site-branding"], "method", false, false, true, 15);
+        // line 16
+        echo "  ";
+        if ((($context["site_logo"] ?? null) || ($context["site_name"] ?? null))) {
+            // line 17
+            echo "    <div class=\"d-inline-flex flex-column justify-content-center align-items-center\">
+      <a class=\"site-branding__name\">
+        ";
+            // line 19
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_name"] ?? null), 19, $this->source), "html", null, true);
+            echo "
+      </a>
+      <div class=\"site-branding__slogan\">
+        ";
+            // line 22
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_slogan"] ?? null), 22, $this->source), "html", null, true);
+            echo "
+      </div>
+    </div>
+  ";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/exam/templates/block--system-branding-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 22,  48 => 19,  44 => 17,  41 => 16,  39 => 15,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/exam/templates/block--system-branding-block.html.twig", "/var/www/exam/web/themes/custom/exam/templates/block--system-branding-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 15, "if" => 16);
+        static $filters = array("escape" => 19);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
